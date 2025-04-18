95ede6864ffcde656e2690c494824ad60613abc1
Jimby Raynor <67350304+JimbyRaynor@users.noreply.github.com>
2025-04-18 13:19:40 +1000

    Add files via upload

--- Centipede.py
+++ Centipede.py
@@ -6,11 +6,17 @@ import sys
 # to import LED
 two_levels_up = os.path.abspath(os.path.join('..', '..'))
 sys.path.insert(0, "/home/deck/Documents")
+import LEDlib
 
 score = 0
 centipedelength = 30
 
-import LEDlib
+
+# The Grid is king. If Grid[x,y] == 2 then there is a centipede there (no other objects!)
+  # Do not allow two objects to be at the same spot.
+  # centipede could hit bullet (sideways)
+# setgrid should report error if there is another object there, ie need grid[x,y]==0
+# check putblock, and move?
 
 # make centipede longer to increase difficulty. Do not make faster
 # only need one centipede list: Just add more sections to list to make more
@@ -24,10 +30,11 @@ os.chdir(current_script_directory)
 SpriteWidth = 32 # height and width of sprites. Every sprite has this size. This is the "block" size
 
 class Spriteobj:
-    def __init__(self, canvas,fup="",fdown="",fright="",fleft="",xblock=0,yblock=0,dx=0,dy=0,size=20):
+    def __init__(self, canvas,fup="",fdown="",fright="",fleft="",xblock=0,yblock=0,dx=0,dy=0,gridtype=0,size=20):
         self.xblock = xblock # number of blocks (sprites) from left of screen # col
         self.yblock = yblock # number of blocks (sprites) down from top of screen # row
         self.size = size
+        self.gridtype = gridtype
         self.dx = dx # 0,1,-1   dx = 1 means move one block right
         self.dy = dy # 0,1,-1   dy = 1 means move one block down
         self.canfire = False;
@@ -56,25 +63,52 @@ canvas1.place(x=0,y=0)
 def creatematrix(rows,cols):
     return [[0 for i in range(cols)] for j in range(rows)]
 
-grid = creatematrix(100,100)   # rows, columns
+Grid = creatematrix(100,100)   # rows, columns
 
 def setgrid(x,y,gtype):
-    grid[y][x] = gtype # matrices refer to y (row number) first!
+    Grid[y][x] = gtype # matrices refer to y (row number) first!
 
 def getgrid(x,y):
-    return grid[y][x] # matrices refer to y (row number) first!
+    return Grid[y][x] # matrices refer to y (row number) first!
 
 def getgridnext(gameobj):
     return getgrid(gameobj.xblock+gameobj.dx,gameobj.yblock+gameobj.dy)
 
+def getgridobj(gameobj):
+    return getgrid(gameobj.xblock,gameobj.yblock)
+
 def setgridnext(gameobj,gtype):
     setgrid(gameobj.xblock+gameobj.dx,gameobj.yblock+gameobj.dy,gtype)
 
+def setgridobj(gameobj,gtype):
+    setgrid(gameobj.xblock,gameobj.yblock,gtype)
+
 def putblock(x,y,stype,dx=0,dy=0,gridtype=0):
-    block = Spriteobj(canvas1,fup=stype,xblock=x,yblock=y,size=SpriteWidth,dx=dx,dy=dy)
-    playfield.append(block)  # to stop garbage collector removing block!
-    setgrid(x,y,gridtype)
-    return block
+    if getgrid(x,y) == 0:  # only one object at each location
+         block = Spriteobj(canvas1,fup=stype,xblock=x,yblock=y,size=SpriteWidth,dx=dx,dy=dy,gridtype=gridtype)
+         playfield.append(block)  # to stop garbage collector removing block!
+         setgrid(x,y,gridtype)
+         return block
+    else:
+        return -1 # also need to fix move() and goto() !! Look at methods for SpriteObj
+
+def blockmove(gameobj):
+    if getgridnext(gameobj) == 0:  # only one object at each location
+         setgridnext(gameobj,gameobj.gridtype)
+         setgridobj(gameobj,0)
+         gameobj.move()
+         return 0
+    else:
+        return -1
+    
+def blockgoto(gameobj,x,y):
+    if getgrid(x,y) == 0:  # only one object at each location
+         setgridobj(gameobj,0)
+         gameobj.goto(x,y)
+         setgrid(x,y,gameobj)
+         return 0
+    else:
+         return -1 
 
 def getblock(x,y):
     for block in playfield:
@@ -121,10 +155,16 @@ def movebody():
 
 def removeblocknext(gameobj):
     myblock = getblocknext(gameobj) 
-    if myblock != -1:
+    while myblock != -1:
         myblock.undraw()
         playfield.remove(myblock)
-        setgridnext(gameobj,0) 
+        myblock = getblocknext(gameobj) 
+    setgridnext(gameobj,0) 
+
+def removeblock(gameobj):
+      gameobj.undraw()
+      setgrid(gameobj.xblock,gameobj.yblock,0)
+      playfield.remove(gameobj)
 
 def addtoscore(amount):
     global LEDscore, score
@@ -139,6 +179,7 @@ def centipedetimer():
     mainwin.after(300,centipedetimer)
 
 def bullettimer():
+    if len(bullets) == 0: return
     for bullet in bullets.copy():
       if (getgridnext(bullet) == 0) and (bullet.yblock > 1):
          bullet.move()
@@ -196,10 +237,23 @@ def mykey(event):
         ship.dx = 0
     elif key == "space":
         if ship.canfire:
-           bullet = putblock(ship.xblock,ship.yblock-1,"bullet.png",dx=0,dy=-1)
-           bullets.append(bullet)
+           blockabove =  getblock(ship.xblock,ship.yblock-1)
+           if blockabove == -1:
+             bullet = putblock(ship.xblock,ship.yblock-1,"bullet.png",dx=0,dy=-1)
+             bullets.append(bullet)
+             mainwin.after(30,bullettimer) 
+           else:
+             if getgridobj(blockabove) == 1:
+                removeblock(blockabove) 
+                addtoscore(1)
+             if getgridobj(blockabove) == 2: # hit centipede
+                centipede.remove(blockabove)
+                removeblock(blockabove) # this will remove centipede part from playfield
+                putblock(blockabove.xblock,blockabove.yblock,"rock.png",gridtype=1)
+                addtoscore(10)
            ship.canfire = False;
-           mainwin.after(200,reload)
+           mainwin.after(400,reload)
+          
 
 mainwin.bind("<KeyPress>", mykey)
 
